afcce8994fe8836f0f5455269d7f75946e36d8d7
Tobiáš Potoček <tobik@google.com>
2017-02-04 22:45:33 +0100

    Showing a balloon with the current country

--- country-guard.py
+++ country-guard.py
@@ -4,41 +4,74 @@ from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
 from urllib2 import Request, urlopen, URLError
 from gi.repository import Notify as notify
+import threading
 
-APPINDICATOR_ID = 'county-guard'
+APPINDICATOR_ID = "county-guard"
+UNKNOWN_COUNTRY_CODE = "?"
+
+countryCode = UNKNOWN_COUNTRY_CODE
 
 def main():
     indicator = appindicator.Indicator.new(
         APPINDICATOR_ID,
-        'whatever',
+        "whatever",
         appindicator.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
     indicator.set_menu(build_menu())
     notify.init(APPINDICATOR_ID)
+    set_interval(check_country, 2)
     gtk.main()
 
 def build_menu():
     menu = gtk.Menu()
 
-    item_quit = gtk.MenuItem('Quit')
-    item_quit.connect('activate', quit)
+    item_quit = gtk.MenuItem("Quit")
+    item_quit.connect("activate", quit)
     menu.append(item_quit)
 
-    item_joke = gtk.MenuItem('Joke')
-    item_joke.connect('activate', joke)
-    menu.append(item_joke)
+    # item_joke = gtk.MenuItem("Joke")
+    # item_joke.connect("activate", joke)
+    # menu.append(item_joke)
 
     menu.show_all()
     return menu
 
-def fetch_joke():
-    request = Request('http://api.icndb.com/jokes/random?limitTo=[nerdy]')
-    response = urlopen(request)
-    joke = json.loads(response.read())['value']['joke']
-    return joke
+def check_country():
+    global countryCode
+    try:
+        ip_info = fetch_ip_info()
 
-def joke(_):
-    notify.Notification.new("<b>Joke</b>", fetch_joke(), None).show()
+        if ip_info["countryCode"] != countryCode:
+            countryCode = ip_info["countryCode"]
+            notify_country(ip_info["country"], ip_info["query"])
+
+    except Exception:
+        if countryCode != UNKNOWN_COUNTRY_CODE:
+            countryCode = UNKNOWN_COUNTRY_CODE
+            notify_failure()
+
+def fetch_ip_info():
+    request = Request("http://ip-api.com/json/")
+    response = urlopen(request)
+    return json.loads(response.read())
+
+def notify_country(country, ip):
+    notify.Notification.new(
+        "<b>You're now in %s</b>" % country,
+        "Your IP: %s" % ip,
+        None).show()
+
+def notify_failure():
+    notify.Notification.new(
+        "Unable to get current IP information!", None, None).show()
+
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
 
 def quit(source):
     notify.uninit()
