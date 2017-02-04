5fc69bcc2222581c83a1ca1045f3522500eed3e0
Tobiáš Potoček <tobik@google.com>
2017-02-04 21:38:41 +0100

    Showing baloons with fetched joke

--- country-guard.py
+++ country-guard.py
@@ -1,6 +1,9 @@
 import signal
+import json
 from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
+from urllib2 import Request, urlopen, URLError
+from gi.repository import Notify as notify
 
 APPINDICATOR_ID = 'county-guard'
 
@@ -11,17 +14,34 @@ def main():
         appindicator.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
     indicator.set_menu(build_menu())
+    notify.init(APPINDICATOR_ID)
     gtk.main()
 
 def build_menu():
     menu = gtk.Menu()
+
     item_quit = gtk.MenuItem('Quit')
     item_quit.connect('activate', quit)
     menu.append(item_quit)
+
+    item_joke = gtk.MenuItem('Joke')
+    item_joke.connect('activate', joke)
+    menu.append(item_joke)
+
     menu.show_all()
     return menu
 
+def fetch_joke():
+    request = Request('http://api.icndb.com/jokes/random?limitTo=[nerdy]')
+    response = urlopen(request)
+    joke = json.loads(response.read())['value']['joke']
+    return joke
+
+def joke(_):
+    notify.Notification.new("<b>Joke</b>", fetch_joke(), None).show()
+
 def quit(source):
+    notify.uninit()
     gtk.main_quit()
 
 if __name__ == "__main__":
