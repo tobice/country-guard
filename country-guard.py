5bf97986ae89fb94e8d279517c5189f34a6cf47a
Tobiáš Potoček <tobik@google.com>
2017-02-04 21:31:42 +0100

    It is now possible to quit the app

--- country-guard.py
+++ country-guard.py
@@ -1,3 +1,4 @@
+import signal
 from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
 
@@ -9,8 +10,20 @@ def main():
         'whatever',
         appindicator.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
-    indicator.set_menu(gtk.Menu())
+    indicator.set_menu(build_menu())
     gtk.main()
 
+def build_menu():
+    menu = gtk.Menu()
+    item_quit = gtk.MenuItem('Quit')
+    item_quit.connect('activate', quit)
+    menu.append(item_quit)
+    menu.show_all()
+    return menu
+
+def quit(source):
+    gtk.main_quit()
+
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
     main()
