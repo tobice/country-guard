a93d4f4506e40f27fd2b68445b7fb0c612e6c4b4
Tobiáš Potoček <tobik@google.com>
2017-02-04 21:26:19 +0100

    init commit

---
+++ country-guard.py
@@ -0,0 +1,16 @@
+from gi.repository import Gtk as gtk
+from gi.repository import AppIndicator3 as appindicator
+
+APPINDICATOR_ID = 'county-guard'
+
+def main():
+    indicator = appindicator.Indicator.new(
+        APPINDICATOR_ID,
+        'whatever',
+        appindicator.IndicatorCategory.SYSTEM_SERVICES)
+    indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+    indicator.set_menu(gtk.Menu())
+    gtk.main()
+
+if __name__ == "__main__":
+    main()
